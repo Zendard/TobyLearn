03c9e4b06b422255d83e41adf90a773fbd6d62c0
Zendard <thijsmuylaert@hotmail.com>
2023-12-20 14:44:52 +0100

    Add functionality  to scan directory for .tl files and convert into sets

--- index.js
+++ index.js
@@ -1,30 +1,77 @@
 const { app, BrowserWindow } = require('electron/main')
+const ejse = require('ejs-electron')
+const fs = require('fs');
+const path = require('path');
+const setsPath = './sets/';
 if (require('electron-squirrel-startup')) app.quit();
 try {
 	require('electron-reloader')(module)
-  } catch (_) {}
+} catch (_) { }
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-  })
-win.removeMenu()
-  win.loadFile('index.html')
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+	})
+	win.removeMenu()
+	win.loadURL('file://' + __dirname + '/views/index.ejs')
 }
 
 app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
+	getSets()
+  .then(sets => {
+    ejse.data('sets', sets)
   })
+  .catch(error => {
+    console.error('Error getting sets:', error);
+  });
+	createWindow()
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	})
 })
 
+function getSets() {
+	return new Promise((resolve, reject) => {
+		let sets = {};
+		fs.readdir(setsPath, (err, files) => {
+			if (err) {
+				console.error('Error reading directory:', err);
+				reject(err);
+				return;
+			}
+			const tlFiles = files.filter(file => path.extname(file) === '.tl');
+			const readFilePromises = tlFiles.map(file => {
+				return new Promise((resolve, reject) => {
+					fs.readFile(setsPath + file, 'utf8', (err, data) => {
+						if (err) {
+							console.error('Error reading file:', err);
+							reject(err);
+							return;
+						}
+						const dictionary = JSON.parse(data);
+						sets[path.basename(file, '.tl')] = dictionary;
+						sets['toby'] = 'hond';
+						resolve();
+					});
+				});
+			});
+			Promise.all(readFilePromises)
+				.then(() => {
+					resolve(sets);
+				})
+				.catch(error => {
+					reject(error);
+				});
+		});
+	});
+}
+
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
