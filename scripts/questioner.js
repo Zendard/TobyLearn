975b6b5526a33913c9e3c803079162f4c2461249
Zendard <thijsmuylaert@hotmail.com>
2023-12-21 18:31:39 +0100

    Add correct visual feedback

--- scripts/questioner.js
+++ scripts/questioner.js
@@ -11,14 +11,30 @@ const randomList = Object.fromEntries(
 let counter = 0;
 questionText.textContent = Object.keys(randomList)[counter];
 
-checkButton.addEventListener("click", (e) => {
+checkButton.addEventListener("click", checkAnswer);
+answerInput.addEventListener("keypress", (e) => {
+	if (e.key === "Enter") {
+		checkAnswer();
+	}
+});
+
+function checkAnswer(e) {
 	console.log(answerInput.value);
 	console.log(randomList[Object.keys(randomList)[counter]]);
 	if (answerInput.value == randomList[Object.keys(randomList)[counter]]) {
 		console.log("Correct!");
+		checkButton.querySelector("i").classList.replace("fa-question", "fa-check");
+		checkButton.classList.replace("neutral", "correct");
+		setTimeout(() => {
+			checkButton
+				.querySelector("i")
+				.classList.replace("fa-check", "fa-question");
+			checkButton.classList.replace("correct", "neutral");
+		}, 600);
 	} else {
 		console.log("wrong!");
 	}
 	counter++;
 	questionText.textContent = Object.keys(randomList)[counter];
-});
+	answerInput.value = "";
+}
