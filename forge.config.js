38e110e468b81bd13ec95b5af328f104916d134c
Zendard <thijsmuylaert@hotmail.com>
2023-12-22 15:14:29 +0100

    Add icons

--- forge.config.js
+++ forge.config.js
@@ -1,30 +1,37 @@
 module.exports = {
-  packagerConfig: {
-    asar: true,
-  },
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-  ],
+	packagerConfig: {
+		icon: "./iconTL",
+		asar: true,
+	},
+	rebuildConfig: {},
+	makers: [
+		{
+			name: "@electron-forge/maker-squirrel",
+			config: {
+				setupIcon: "./iconTL.ico",
+			},
+		},
+		{
+			name: "@electron-forge/maker-zip",
+			platforms: ["darwin"],
+		},
+		{
+			name: "@electron-forge/maker-deb",
+			config: {
+				options: {
+					icon: "./iconTL.png",
+				},
+			},
+		},
+		{
+			name: "@electron-forge/maker-rpm",
+			config: {},
+		},
+	],
+	plugins: [
+		{
+			name: "@electron-forge/plugin-auto-unpack-natives",
+			config: {},
+		},
+	],
 };
--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const createWindow = () => {
 	const win = new BrowserWindow({
 		width: 800,
 		height: 600,
+		icon: "/path/to/icon.png",
 		webPreferences: {
 			preload: path.join(__dirname, "preload.js"),
 		},
