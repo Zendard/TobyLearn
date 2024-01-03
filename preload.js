73b934adda839b98f07edaf88e1ef12fea4679a8
Zendard <thijsmuylaert@hotmail.com>
2024-01-03 13:55:24 +0100

    Clear everything

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron/renderer");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-	getSets: () => ipcRenderer.invoke("getSets"),
-	openFile: () => ipcRenderer.invoke("dialog:openFile"),
-	makeSet: (formData) => ipcRenderer.send("makeSet", formData),
-	deleteSet: (set) => ipcRenderer.send("deleteSet", set),
-	editSet: (set) => ipcRenderer.send("editSet", set),
-});
--- scripts/autoGrowInput.js
+++
@@ -1,7 +0,0 @@
-const inputList = document.querySelectorAll("input");
-
-inputList.forEach((inputElement) => {
-	inputElement.addEventListener("input", () => {
-		inputElement.style.width = inputElement.value.length + 1 + "rem";
-	});
-});
