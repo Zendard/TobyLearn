16893efecd1f3a849515213fa356fa428cffdc83
Zendard <thijsmuylaert@hotmail.com>
2024-01-03 16:57:42 +0100

    Add get_file_content handler

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,11 +2,14 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use serde_json::json;
 use std::fs;
-fn get_files() -> Result<String, Box<dyn std::error::Error>> {
-    let folder_path: &str = "/home/zendard/Downloads";
+use std::path::Path;
+use tauri::Result;
+
+const FOLDER_PATH: &str = "/home/zendard/Downloads";
+fn get_files() -> Result<String> {
     let mut file_data: serde_json::Value = json!({});
 
-    for entry in fs::read_dir(folder_path)? {
+    for entry in fs::read_dir(FOLDER_PATH)? {
         let entry: fs::DirEntry = entry?;
         let file_path: std::path::PathBuf = entry.path();
 
@@ -39,10 +42,16 @@ fn get_sets() -> String {
         }
     }
 }
+#[tauri::command]
+fn get_file_content(file_name: String) -> Result<String> {
+    let file_content = fs::read_to_string(Path::new(FOLDER_PATH).join(file_name + ".tl"))?;
+    Ok(file_content)
+}
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![get_sets])
+        .invoke_handler(tauri::generate_handler![get_file_content])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
