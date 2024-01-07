74c6888f6bf9970567ace8df864400a74e1ef993
Zendard <thijsmuylaert@hotmail.com>
2024-01-07 13:23:01 +0100

    Add button animations

--- components/questioner.tsx
+++ components/questioner.tsx
@@ -2,7 +2,7 @@ import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
 import { invoke } from '@tauri-apps/api'
-import { useState,useEffect, SetStateAction, Dispatch, createRef, useRef, MutableRefObject } from 'react'
+import { useState,useEffect, SetStateAction, Dispatch} from 'react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { UseFormReturn, useForm } from 'react-hook-form'
 import * as z from 'zod'
@@ -25,14 +25,14 @@ export function Questioner({currentSet,setError}:{currentSet:string,setError:(ar
 		},
 	})
 
-	const buttonRef=useRef(null)
+	const [buttonClass,setButtonClass]=useState('')
 	
 	return(
 		<div className="questioner flex flex-col gap-5">
 			<h1 id="question" className="text-7xl">{Object.keys(fileContent)[questionCounter]}</h1>
 			<Form {...form}>
 				<form className="flex gap-3" onSubmit={
-					form.handleSubmit((data)=>checkAnswer(questionCounter,fileContent,data,setQuestionCounter,form,buttonRef))
+					form.handleSubmit((data)=>checkAnswer(questionCounter,fileContent,data,setQuestionCounter,form,setButtonClass))
 				}>
 					<FormField
 						control={form.control}
@@ -45,7 +45,7 @@ export function Questioner({currentSet,setError}:{currentSet:string,setError:(ar
 							</FormItem>
 						)}
 					/>
-					<Button ref={buttonRef}>Ok</Button>
+					<Button className={buttonClass}>Ok</Button>
 				</form>
 			</Form>
 		</div>
@@ -65,17 +65,18 @@ const FormSchema = z.object({
 	answer: z.string(),
 })
 
-function checkAnswer(questionCounter:number,fileContent:set,data: z.infer<typeof FormSchema>,setQuestionCounter: Dispatch<SetStateAction<number>>,form: UseFormReturn<{ answer: string }, any, undefined>,buttonRef: MutableRefObject<null>){
+function checkAnswer(questionCounter:number,fileContent:set,data: z.infer<typeof FormSchema>,setQuestionCounter: Dispatch<SetStateAction<number>>,form: UseFormReturn<{ answer: string }, any, undefined>,setButtonClass: Dispatch<SetStateAction<string>>){
 	console.log(data.answer)
 	console.log(fileContent[Object.keys(fileContent)[questionCounter]])
 	if(data.answer == fileContent[Object.keys(fileContent)[questionCounter]]){
 		console.log('juist')
+		setButtonClass('correct')
+		setTimeout(()=>setButtonClass(''),1000)
 	}else{
 		console.log('fout')
+		setButtonClass('wrong')
+		setTimeout(()=>setButtonClass(''),1000)
 	}
 	form.setValue('answer','')
-	const button=buttonRef.current
-	console.log(button)
-	button.classList.add('bg-green')
 	setQuestionCounter(questionCounter+1)
 }
