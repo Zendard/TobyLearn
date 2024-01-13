9663c5c5cb0afef2dab2a25f73e7b28d5e2a2fe0
Zendard <thijsmuylaert@hotmail.com>
2024-01-13 19:24:47 +0100

    Try to add themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 					attribute="class"
 					defaultTheme="system"
 					enableSystem
-					disableTransitionOnChange
+					themes={['white','blue','red']}
 				>
 					{children}
 				</ThemeProvider>
