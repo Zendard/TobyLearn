af13e4f210ccce346c7d1d533322f5eb1353d8b3
Zendard <thijsmuylaert@hotmail.com>
2024-01-21 12:49:51 +0100

    Save settings

--- components/settings.tsx
+++ components/settings.tsx
@@ -21,12 +21,14 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import {invoke} from '@tauri-apps/api/tauri'
+import { useToast } from './ui/use-toast'
 
 
   
   
 
 export function Settings(){
+	const {toast}=useToast()
 
 	const settingsSchema = z.object({
 		accentColor: z.string(),
@@ -42,7 +44,7 @@ export function Settings(){
 
 	function onSubmit(values: z.infer<typeof settingsSchema>) {
 		console.log(values)
-		invoke<string>('save_settings')
+		invoke<string>('save_settings',{'settings':JSON.stringify(values)}).then((successString)=>toast({title:successString}))
 	}
 
 	return(
