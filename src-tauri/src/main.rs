ca650e3e47d357c7be88b22ae8cdd4766b74da59
Zendard <thijsmuylaert@hotmail.com>
2024-02-05 18:50:07 +0100

    Create settings file when it does not exist

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -105,6 +105,15 @@ fn get_settings() -> Result<String, String> {
         Ok(data) => data,
     };
     let settings_file = proj_dirs.preference_dir().join("settings.json");
+    if !settings_file.is_file() {
+        match fs::write(
+            &settings_file,
+            "{\"accentColor\":\"purple\",\"randomizeQuestions\":true}",
+        ) {
+            Ok(_data) => println!("LOG: created settings.json"),
+            Err(e) => return Err(format!("Error while creating settings file: {e}")),
+        }
+    }
 
     let settings = match fs::read(settings_file) {
         Err(e) => return Err(format!("Error while reading settings.json: {e}")),
