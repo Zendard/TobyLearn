494a52223ec9863c5afe5c3e6093bf8017689b7a
Zendard <thijsmuylaert@hotmail.com>
2024-02-11 23:36:11 +0100

    Fix crash when cancelling export

--- src-tauri/src/export_set.rs
+++ src-tauri/src/export_set.rs
@@ -6,12 +6,13 @@ use tauri::api::dialog::blocking::FileDialogBuilder;
 pub fn export_set(set_name: String) -> Result<String, String> {
     let save_path = FileDialogBuilder::new()
         .set_title("Choose export location")
-        .save_file()
-        .unwrap();
-    if !save_path.is_dir() {
+        .set_file_name(&set_name)
+        .add_filter("TobyLearn Sets (.tl)", &["tl"])
+        .save_file();
+    if save_path.is_none() {
         return Ok("Cancelled export".to_string());
     }
-    let save_path = save_path.join(set_name.clone() + ".tl");
+    let save_path = save_path.unwrap().join(set_name.clone() + ".tl");
 
     let set_content = match get_file_content(set_name.clone()) {
         Err(e) => return Err(e),
